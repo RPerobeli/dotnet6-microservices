d17ffe8c35680cf57a184ffb20859641147f46c3
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2023-08-31 07:34:24 -0300

    feat: preparing the api for database using entity and MySql

---
+++ GeekShopping/GeekShopping.Product.API/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.Product.API.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.Product.API.DependencyInjection
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddMySqlDbContext(this WebApplicationBuilder builder)
+        {
+            var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+            builder.Services.AddDbContext<MySqlContext> (options => options.
+                UseMySql(connection,
+                        new MySqlServerVersion(
+                            new Version(8,0,32)
+                            )
+                        )
+                );
+            return builder;
+        }
+    }
+}
