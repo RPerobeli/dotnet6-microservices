77faeb156f936a127c7ae4fbae5c11aebf15518e
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2025-02-01 08:39:07 -0300

    aula 8 finalizada

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GeekShopping.Web
+namespace GeekShopping.ProductAPI
 {
     public class Program
     {
